200b36e9eb0539c6a73820288a1dc0cb1c290a81
Даниил Полянский <dm.tsiganov@gmail.com>
2022-11-22 21:07:06 +0700

    3

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Введите цифру, обозначающую день недели: ");
+int dayNumber = Convert.ToInt32(Console.ReadLine());
+  if (dayNumber == 6 || dayNumber == 7)
+    {  
+     Console.WriteLine("Этот день выходной");
+    }
+  else Console.WriteLine("Этот день не выходной");
+
+
+
+
+
+
